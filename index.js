72e0069ad70924115bf008ec2985469987270a05
Xitef156 <77064386+Xitef156@users.noreply.github.com>
2021-09-26 11:17:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const ffmpeg = require('fluent-ffmpeg');require('ffmpeg-static');
 const NodeID3 = require('node-id3');
 const ncu = require('npm-check-updates');
 var Download = require('image-downloader');
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
 
 const SC = new SoundCloud.Client();
 const Instent = Discord.Intents.FLAGS
